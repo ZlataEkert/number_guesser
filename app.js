14b5a25f0ac53c15fc2a5ec92af06e3aa1670b31
ZlataEkert <zlataekert@gmail.com>
2022-09-02 16:46:02 +0300

    add a 'play again option' & random number generator

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ GAME FUNCTION:
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI Elements
-const gameWrapper = document.querySelector('#game'),
+const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
@@ -26,16 +26,15 @@ minNum.textContent = min;
 maxNum.textContent = max;
 
 // Play again event listener
-gameWrapper.addEventListener('click', function(e){
+game.addEventListener('mousedown', function(e){
   if(e.target.className === 'play-again'){
-
+    window.location.reload();
   }
 });
 
 // Listen for guess
 guessBtn.addEventListener('click', function(){
   let guess = parseInt(guessInput.value);
-  console.log(guess);
 
   // Validate
   if(isNaN(guess) || guess < min || guess > max){
@@ -87,6 +86,11 @@ function gameOver(won, msg){
   // Play again
   guessBtn.value = 'Play Again';
   guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max){
+ return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // Set message
